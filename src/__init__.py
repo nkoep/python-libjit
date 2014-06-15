nkoep/python-libjit

commit 79f89dd43dcd898948e4062ca72e605b55a99f57

Add closure support for system and aggregate types
Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

src/__init__.py
@@ -69,20 +69,33 @@ def builds_function(context, signature):
         return wrapper
     return decorator
 
+def _determine_nint_type(**kwargs):
+    if not (len(kwargs) == 1 and "signed" in kwargs):
+        raise ValueError("need keyword argument 'signed'")
+    signed = kwargs["signed"]
+    pointer_size = ctypes.sizeof(ctypes.c_void_p)
+    if pointer_size == 4:
+        return ctypes.c_int32 if signed else ctypes.c_uint32
+    elif pointer_size == 8:
+        return ctypes.c_int64 if signed else ctypes.c_uint64
+    raise TypeError("unsupported pointer size")
+
 class Closure(_CFuncPtr):
     _flags_ = _FUNCFLAG_CDECL
 
     _LIBJIT_TO_CTYPES = {
+        # Platform-agnostic types
         Type.VOID: None,
         Type.SBYTE: ctypes.c_byte,
         Type.UBYTE: ctypes.c_ubyte,
         Type.USHORT: ctypes.c_ushort,
-        Type.SHORT: ctypes.c_short,
-        Type.INT: ctypes.c_int,
-        Type.UINT: ctypes.c_uint,
-        # XXX: These are either int or long, but which is it?
-        # Type.NINT: ...,
-        # Type.NUINT: ...,
+        Type.SHORT: ctypes.c_int16,
+        Type.INT: ctypes.c_int32,
+        Type.UINT: ctypes.c_uint32,
+        # `nint' represents types with the same width and alignment as pointers
+        # on the current platform.
+        Type.NINT: _determine_nint_type(signed=True),
+        Type.NUINT: _determine_nint_type(signed=False),
         Type.LONG: ctypes.c_long,
         Type.ULONG: ctypes.c_ulong,
         Type.FLOAT32: ctypes.c_float,
@@ -90,31 +103,23 @@ class Closure(_CFuncPtr):
         Type.NFLOAT: ctypes.c_longdouble,
         Type.VOID_PTR: ctypes.c_void_p,
 
-        # TODO: Add system types.
-        Type.SYS_BOOL: ctypes.c_bool
+        # System types
+        Type.SYS_BOOL: ctypes.c_bool,
+        Type.SYS_CHAR: ctypes.c_char,
+        Type.SYS_SCHAR: ctypes.c_byte,
+        Type.SYS_UCHAR: ctypes.c_ubyte,
+        Type.SYS_SHORT: ctypes.c_short,
+        Type.SYS_USHORT: ctypes.c_ushort,
+        Type.SYS_INT: ctypes.c_int,
+        Type.SYS_UINT: ctypes.c_uint,
+        Type.SYS_LONG: ctypes.c_long,
+        Type.SYS_ULONG: ctypes.c_ulong,
+        Type.SYS_LONGLONG: ctypes.c_longlong,
+        Type.SYS_ULONGLONG: ctypes.c_ulonglong,
+        Type.SYS_FLOAT: ctypes.c_float,
+        Type.SYS_DOUBLE: ctypes.c_double,
+        Type.SYS_LONG_DOUBLE: ctypes.c_longdouble
     }
-
-    """
-    c_bool	_Bool	bool (1)
-    c_char	char	1-character string
-    c_wchar	wchar_t	1-character unicode string
-    c_byte	char	int/long
-    c_ubyte	unsigned char	int/long
-    c_short	short	int/long
-    c_ushort	unsigned short	int/long
-    c_int	int	int/long
-    c_uint	unsigned int	int/long
-    c_long	long	int/long
-    c_ulong	unsigned long	int/long
-    c_longlong	__int64 or long long	int/long
-    c_ulonglong	unsigned __int64 or unsigned long long	int/long
-    c_float	float	float
-    c_double	double	float
-    c_longdouble	long double	float
-    c_char_p	char * (NUL terminated)	string or None
-    c_wchar_p	wchar_t * (NUL terminated)	unicode or None
-    c_void_p	void *	int/long or None
-    """
 
     def __new__(cls, function):
         if not isinstance(function, Function):
@@ -142,12 +147,20 @@ class Closure(_CFuncPtr):
                 return self._LIBJIT_TO_CTYPES[type_]
             except KeyError:
                 pass
-        # TODO: These two need to recurse over their fields.
+        elif type_.is_pointer():
+            ref_type = type_.get_ref()
+            return ctypes.POINTER(self._convert_libjit_to_ctypes(ref_type))
         elif type_.is_struct():
-            raise NotImplementedError
+            self._create_aggregate_type(ctypes.Structure, type_)
         elif type_.is_union():
-            raise NotImplementedError
+            self._create_aggregate_type(ctypes.Union, type_)
         raise ValueError(
             "failed to determine ctypes equivalent of jit.Type '%s'" %
             type_.get_name())
+
+    def _create_aggregate_type(self, base_class, type_):
+        fields = [self._convert_libjit_to_ctypes(type_.get_field(i))
+                  for i in range(type_.num_fields())]
+        type_name = "struct" if base_class is ctypes.Structure else "union"
+        return type(type_name, (base_class,), {"_fields_": fields})
 

src/pyjit-type.c
@@ -764,6 +764,9 @@ do {                                                                \
         return -1;                                                  \
 } while (0)
 
+    /* Platform-agnostic types, e.g. jit_type_int always describes 32-bit
+     * wide signed integers
+     */
     REGISTER_TYPE(void);
     REGISTER_TYPE(sbyte);
     REGISTER_TYPE(ubyte);
@@ -780,6 +783,9 @@ do {                                                                \
     REGISTER_TYPE(nfloat);
     REGISTER_TYPE(void_ptr);
 
+    /* System types, e.g. jit_type_sys_int may describe a 32- or 64-bit signed
+     * integer depending on the platform's architecture
+     */
     REGISTER_TYPE(sys_bool);
     REGISTER_TYPE(sys_char);
     REGISTER_TYPE(sys_schar);

commit 9eed723fac4ef6b6691a6f871e29c91a26a473a5

Fix two bugs introduced in 79f89dd
- Closure._convert_libjit_to_ctypes missed two return statements when
  converting aggregate types.
- When creating ctypes wrappers for aggregate types, the `_fields_'
  attribute must be a list of 2-tuples specifying the name and type of
  the individual fields. In the last commit, however, we only assembled
  the types in a list and forgot to give unique field names.
- Testing for primitive types in Closure._convert_libjit_to_ctypes means
  we cannot convert things like jit.Type.SYS_CHAR since LibJIT does not
  consider SYS_* types primitive. We skip the primitivity test altogether now
  to avoid confusion.

Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

src/__init__.py
@@ -141,26 +141,47 @@ class Closure(_CFuncPtr):
         self.argtypes = [self._convert_libjit_to_ctypes(signature.get_param(i))
                          for i in range(signature.num_params())]
 
-    def _convert_libjit_to_ctypes(self, type_):
-        if type_.is_primitive():
-            try:
-                return self._LIBJIT_TO_CTYPES[type_]
-            except KeyError:
-                pass
-        elif type_.is_pointer():
+    @staticmethod
+    def _convert_libjit_to_ctypes(type_):
+        try:
+            return Closure._LIBJIT_TO_CTYPES[type_]
+        except KeyError:
+            pass
+        if type_.is_pointer():
             ref_type = type_.get_ref()
-            return ctypes.POINTER(self._convert_libjit_to_ctypes(ref_type))
+            return ctypes.POINTER(Closure._convert_libjit_to_ctypes(ref_type))
         elif type_.is_struct():
-            self._create_aggregate_type(ctypes.Structure, type_)
+            return Closure._create_aggregate_type(ctypes.Structure, type_)
         elif type_.is_union():
-            self._create_aggregate_type(ctypes.Union, type_)
+            return Closure._create_aggregate_type(ctypes.Union, type_)
+        # TODO: Test if the type is tagged with jit.TYPETAG_NAME to obtain a
+        #       proper name for the exception.
         raise ValueError(
-            "failed to determine ctypes equivalent of jit.Type '%s'" %
-            type_.get_name())
+            "failed to determine ctypes conversion for '%s'" % type_)
 
-    def _create_aggregate_type(self, base_class, type_):
-        fields = [self._convert_libjit_to_ctypes(type_.get_field(i))
-                  for i in range(type_.num_fields())]
+    @staticmethod
+    def _create_aggregate_type(base_class, type_):
+        num_anonymous = 0
+        names = set()
+        fields = []
+        for i in range(type_.num_fields()):
+            name = type_.get_name(i)
+            # If no name was set for the field, use `num_anonymous' instead. To
+            # avoid name collisions, we keep track of already used names.
+            if name is None:
+                while True:
+                    name = str(num_anonymous)
+                    num_anonymous += 1
+                    if name not in names:
+                        break
+            elif name in names:
+                raise ValueError(
+                    "multiple fields with the name '%s' defined" % name)
+            names.add(name)
+
+            field = type_.get_field(i)
+            fields.append((name, Closure._convert_libjit_to_ctypes(field)))
+
         type_name = "struct" if base_class is ctypes.Structure else "union"
         return type(type_name, (base_class,), {"_fields_": fields})
 